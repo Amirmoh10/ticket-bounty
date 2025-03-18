2f6751dca2cde1b8aa908333eb70122475b19a57
Amir Ghezala <mohamedghezala@gmail.com>
2025-03-18 12:32:42 +0400

    add theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import "./globals.css";
 
-import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
 
-import { buttonVariants } from "@/components/ui/button";
-import { homePath, ticketsPath } from "@/paths";
+import { Header } from "@/components/header";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,43 +31,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav
-          className="
-            supports-backdrop-blur:bg-background/60
-            fixed left-0 right-0 top-0 z-20
-            border-b bg-background/95 backdrop-blur
-            w-full flex py-2.5 px-5 justify-between
-          "
-        >
-          <div>
-            <Link
-              href={homePath()}
-              className={buttonVariants({ variant: "ghost" })}
-            >
-              <LucideKanban />
-              <h1 className="text-lg font-semibold">TicketBounty</h1>
-            </Link>
-          </div>
-          <div>
-            <Link
-              href={ticketsPath()}
-              className={buttonVariants({ variant: "default" })}
-            >
-              Tickets
-            </Link>
-          </div>
-        </nav>
-        <main
-          className="
+        <ThemeProvider>
+          <Header />
+          <main
+            className="
             min-h-screen flex-1
             overflow-y-auto overflow-x-hidden
             py-24 px-8
             bg-secondary/20
             flex flex-col
           "
-        >
-          {children}
-        </main>
+          >
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
