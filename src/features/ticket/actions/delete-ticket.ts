0e10ab66e8955df3a4da3f48877ecaaac228c14c
Amir Ghezala <mohamedghezala@gmail.com>
2025-03-23 13:57:39 +0400

    cache on demand

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,9 +1,16 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 import { prisma } from "@/lib/prisma-client";
+import { ticketsPath } from "@/paths";
 
 export const deleteTicket = async (id: string) => {
   await prisma.ticket.delete({
     where: { id },
   });
+
+  revalidatePath(ticketsPath());
+  redirect(ticketsPath());
 };
