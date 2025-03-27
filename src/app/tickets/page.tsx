bba52d98315d4d47eabea5e2625e9e2a1f1294e2
Amir Ghezala <mohamedghezala@gmail.com>
2025-03-27 21:16:40 +0400

    use configuration over composition

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,14 +1,8 @@
 import { Suspense } from "react";
 
+import { CardCompact } from "@/components/card-compact";
 import { Heading } from "@/components/heading";
 import Spinner from "@/components/spinner";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import TicketCreateForm from "@/features/ticket/components/ticketCreateForm";
 import TicketsList from "@/features/ticket/components/tickets-list";
 
@@ -17,15 +11,12 @@ const TicketsPage = () => {
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>A new ticket will be created</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <TicketCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title="Create Ticket"
+        description="A new ticket will be created"
+        className="w-full max-w-[420px] self-center"
+        content={<TicketCreateForm />}
+      />
 
       <Suspense fallback={<Spinner />}>
         <TicketsList />
