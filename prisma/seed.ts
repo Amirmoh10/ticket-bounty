da55746d27ad7ea637432ac1a4b297e7417baf5a
Amir Ghezala <mohamedghezala@gmail.com>
2025-05-16 18:10:04 +0400

    add new deadline and bounty data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,16 +5,22 @@ const tickets = [
     title: "Ticket 1",
     content: "This is 1 ticket from database",
     status: "DONE" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 499,
   },
   {
     title: "Ticket 2",
     content: "This is 2 ticket from database",
     status: "OPEN" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 399,
   },
   {
     title: "Ticket 3",
     content: "This is 3 ticket from database",
     status: "IN_PROGRESS" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 599,
   },
 ];
 
